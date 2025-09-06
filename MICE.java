a9c591b08b157825409e8ffc36eaacee8ed8e079
Omar Elsaghir <Oelsaghir3@gmail.com>
2025-09-06 12:47:29 -0500

    Add files via upload

---
+++ MICE.java
@@ -0,0 +1,20 @@
+import gui.MainWin;
+import gui.Screen;
+import emporium.Emporium;
+import product.MixIn;
+import product.IceCreamFlavor;
+import product.MixInFlavor;
+import product.Scoop;
+import product.MixInAmount;
+import product.Item;
+import product.Container;
+import product.Serving;
+import product.Order;
+
+
+public class MICE {
+    public static void main(String[] args) {
+        MainWin mainWin = new MainWin();
+        //mainwin.setVisible(true);
+    }
+} 
